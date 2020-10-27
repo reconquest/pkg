e8c791a633637b79c6fcbf98f40f14be6120fa64
Egor Kovetskiy <e.kovetskiy@gmail.com>
2020-10-27 11:04:24 +0300

    add dead simple assert package

---
+++ assert/assert.go
@@ -0,0 +1,17 @@
+package assert
+
+import (
+	"fmt"
+)
+
+func NotZero(i int, format string, values ...interface{}) {
+	if i == 0 {
+		panic(fmt.Errorf(format, values...))
+	}
+}
+
+func NotNil(obj interface{}, format string, values ...interface{}) {
+	if obj == nil {
+		panic(fmt.Errorf(format, values...))
+	}
+}
