08ced6ae3ce1557cbcd9f86020da5aa439ac895a
Egor Kovetskiy <e.kovetskiy@gmail.com>
2020-10-28 11:35:58 +0300

    add NotZero64

--- assert/assert.go
+++ assert/assert.go
@@ -10,6 +10,12 @@ func NotZero(i int, format string, values ...interface{}) {
 	}
 }
 
+func NotZero64(i int64, format string, values ...interface{}) {
+	if i == 0 {
+		panic(fmt.Errorf(format, values...))
+	}
+}
+
 func NotNil(obj interface{}, format string, values ...interface{}) {
 	if obj == nil {
 		panic(fmt.Errorf(format, values...))
